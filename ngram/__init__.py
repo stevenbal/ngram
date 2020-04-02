8767ee923cf57457e27a3b0a12564c7f1f47bbd3
Steven <steevbal@hotmail.com>
2020-04-02 15:39:31 +0200

    :sparkles: Added NaiveBayesClassifier

--- ngram/__init__.py
+++ ngram/__init__.py
@@ -1,4 +1,5 @@
 from .language_model import LanguageModel
 from .nested_dict import NestedDict
+from .classifiers import NaiveBayesClassifier
 
-__all__ = ["LanguageModel", "NestedDict"]
+__all__ = ["LanguageModel", "NestedDict", "NaiveBayesClassifier"]

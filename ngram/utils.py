32f961be528581b916f060d254fe5f2c77bed13b
Steven <steevbal@hotmail.com>
2020-04-02 14:14:17 +0200

    :recycle: Refactored preprocessing code

--- ngram/utils.py
+++ ngram/utils.py
@@ -1,7 +1,14 @@
 import re
 
 
-def preprocess_sentence(sentence):
+def preprocess_sentence(
+    sentence,
+    lower=True,
+    remove_links=True,
+    remove_emails=True,
+    break_words=True,
+    alphanumeric_only=True,
+):
     """
     Description:    function that preprocesses a sentence before it
                     is used in a LanguageModel or classified
@@ -13,15 +20,21 @@ def preprocess_sentence(sentence):
     -sentence:      str, the sentence after preprocessing
 
     """
-    sentence = sentence.lower()
-    # Remove website links
-    sentence = re.sub(r"(https?:\/\/(www)?[^\s]*)|(www\.[^\s]*)", "", sentence)
-    # Remove twitter mentions and email addresses
-    sentence = re.sub(r"[^\s]*@[^\s]*", "", sentence)
-    # Break up slashed and hyphenated words
-    sentence = re.sub(r"(?<=[^\s])(\/|-)(?=[^\s])", " ", sentence)
-    # Remove all non alphanumeric characters
-    sentence = re.sub(r"[^a-z0-9\s]", "", sentence)
+    if lower:
+        sentence = sentence.lower()
+    if remove_links:
+        # Remove website links
+        sentence = re.sub(r"(https?:\/\/(www)?[^\s]*)|(www\.[^\s]*)", "", sentence)
+    if remove_emails:
+        # Remove twitter mentions and email addresses
+        sentence = re.sub(r"[^\s]*@[^\s]*", "", sentence)
+    if break_words:
+        # Break up slashed and hyphenated words
+        sentence = re.sub(r"(?<=[^\s])(\/|-)(?=[^\s])", " ", sentence)
+    if alphanumeric_only:
+        # Remove all non alphanumeric characters
+        sentence = re.sub(r"[^a-z0-9\s]", "", sentence)
+        
     # Split and join with a single space between words
     sentence = " ".join(sentence.split())
     return sentence
